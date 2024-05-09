acca9e850ea9d91ea5b093d335bd6518a5431ca1
Adenbroughton <aden.broughton@barclays.com>
2024-05-09 16:58:45 +0100

    created Scooter Class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,61 @@
+const User = require("./User")
+
 class Scooter {
-  // scooter code here
+  constructor (station) {
+    this.station = station
+    this.user = null
+    this.serial = Scooter.nextSerial++
+    this.charge = 100
+    this.isBroken = false
+  }
+
+  static nextSerial = 1
+
+  rent(user) {
+    if (this.charge > 20 && !this.isBroken) {
+      this.user = user
+      this.station = null
+    } else  {
+      if (this.charge <= 20) {
+        throw new Error ("Scooter needs to charge")
+      } else if (this.isBroken) {
+        throw new Error ("Scooter needs repair")
+      }
+    }
+  }
+
+  dock(station) {
+    this.user = null
+    this.station = station
+
+  }
 }
+
+  recharge() {
+ const timer = setInterval() {
+    if (this.charge < 100) {
+      this.charge + 10 
+    }
+    else if (this.charge >= 100) {
+  clearTimeout(timer)
+  }
+}
+  
+  }
+
+
+
+class User{
+  constructor (person){
+    this.person = person
+  }
+}
+
+const scooter1 = new Scooter(london)
+console.log (scooter1)
+
+
+
+
 
 module.exports = Scooter
