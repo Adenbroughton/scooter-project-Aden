8011b022d19dc66f37638404450b527469f8464d
Adenbroughton <aden.broughton@barclays.com>
2024-05-10 14:12:55 +0100

    Fixed Recharge

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,61 +1,52 @@
-const User = require("./User")
-
 class Scooter {
-  constructor (station) {
-    this.station = station
-    this.user = null
-    this.serial = Scooter.nextSerial++
-    this.charge = 100
-    this.isBroken = false
+  constructor(station) {
+    this.station = station;
+    this.user = null;
+    this.serial = Scooter.nextSerial++;
+    this.charge = 100;
+    this.isBroken = false;
   }
 
-  static nextSerial = 1
+  static nextSerial = 1;
 
   rent(user) {
     if (this.charge > 20 && !this.isBroken) {
-      this.user = user
-      this.station = null
-    } else  {
+      this.user = user;
+      this.station = null;
+    } else {
       if (this.charge <= 20) {
-        throw new Error ("Scooter needs to charge")
+        throw new Error("Scooter needs to charge");
       } else if (this.isBroken) {
-        throw new Error ("Scooter needs repair")
+        throw new Error("Scooter needs repair");
       }
     }
   }
 
   dock(station) {
-    this.user = null
-    this.station = station
-
+    this.user = null;
+    this.station = station;
   }
-}
 
   recharge() {
- const timer = setInterval() {
-    if (this.charge < 100) {
-      this.charge + 10 
-    }
-    else if (this.charge >= 100) {
-  clearTimeout(timer)
+    const chargingTimer = setInterval(() => {
+      if (this.charge < 100) {
+        this.charge += 10;
+        console.log("Charge has increase by 10%");
+      } else if (this.charge >= 100) {
+        console.log("Charging complete")
+        clearTimeout(chargingTimer);
+      }
+    }, 1000);
   }
-}
-  
-  }
+   }
 
 
-
-class User{
-  constructor (person){
-    this.person = person
-  }
-}
-
-const scooter1 = new Scooter(london)
-console.log (scooter1)
+const scooter1 = new Scooter("london");
+scooter1.charge = 50
+scooter1.recharge();
 
 
 
 
 
-module.exports = Scooter
+module.exports = Scooter;
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -1,7 +1,77 @@
-// require the User and Scooter classes - see where they can be used in ScooterApp.js
+const User = require("./User")
+const Scooter = require("./User")
 
 class ScooterApp {
-  // ScooterApp code here
+constructor () {
+  this.station = {
+    "Northwich": [],
+    "Hartford": [],
+    "Greenbank": []
+  }
+  this.registeredUsers = {}
 }
+
+
+
+  registerUser(username, password, age) {
+    if (this.registeredUsers[username]) {
+      throw new Error("User already registered.");
+    }
+    if (age < 18) {
+      throw new Error("User is too young to register.");
+    }
+    const user = new User(username, password, age);
+    this.registeredUsers[username] = user;
+    console.log(`${username} has been registered.`);
+    return user;
+}
+  
+
+  loginUser(username, password) {
+      User.login(password)
+      if (true) 
+      console.log(`${username} user has been logged in.`);
+    if (!User || User.password !== password) {
+      throw new Error("Username or password is incorrect.");
+
+}
+  }
+
+  logoutUser(username) {
+User.logout()
+console.log(`${username} user is logged out`);
+if (!User.logout) {
+  console.log(`${username} no such user is logged in`)
+}
+  }
+
+  createScooter(station) {
+let scooter = new Scooter()
+this.station.push(scooter)
+  }
+
+//   This method is called by the Scooter company’s home office when new scooters are deployed.
+// Create a new scooter, add it to the station’s scooter list, and set its station property. Log created new scooter to the console. Return the scooter.
+// Throws no such station error if the station does not exist.
+
+  dockScooter(scooter, station) {
+
+  }
+
+  rentScooter(scooter, user) {
+
+  }
+
+  print() {
+
+  }
+
+
+
+
+}
+
+User
+Scooter
 
 module.exports = ScooterApp
